4daab49ad21e1ff705027ba6ca21806fa4d5ff9e
marceph <marcemtz321@gmail.com>
2022-08-19 18:30:46 -0300

    probando js

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+let nombre = prompt("Ingrese su nombre: ");
+let edad = prompt("Ingrese su edad ");
+
+if(edad >= 18){
+    alert(`Hola ${nombre}, usted es mayor de edad`);
+} else {
+    alert(`Hola ${nombre}, usted no es mayor de edad`);
+}
+
+
+/*let numero1 = numero % 5;
+alert(numero1);
+alert("Hola " + nombre);
+alert(`soy ${numero} y estoy caminando`);
+*/
+
+
+
+/*
+Por último debemos tener en cuenta que como cualquier otro lenguaje de 
+programación, JS también usa palabras reservadas (palabras clave) que 
+no podrán utilizarse por los programadores para usarlas como identificadores. 
+Algunos ejemplo de ellas son: await, break, case, catch, class, const, 
+continue, debugger, default, delete, do, else, enum, export, extensions, 
+false, finally, for, function, if, import, in, instanceof, new, null, 
+return, super, switch, this, throw, true, try, typeof, var, void, 
+while, with, yield, etc.
+*/
